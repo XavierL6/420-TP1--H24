2e1162fad7412ef1226f1b1246957ca1235b3a93
XavierL6 <popfizz10@outlook.com>
2024-02-24 15:06:22 -0500

    Deuxieme commit. Version Composant fait et config moitie

--- src/Composant.java
+++ src/Composant.java
@@ -1,3 +1,69 @@
 public class Composant {
-    
+    private String categorie;
+    private String marque;
+    private String nom;
+    private double prix;
+    private double rabais;
+    public Composant(String pcategorie, String pmarque, String pnom, double pprix){
+        this.categorie = pcategorie;
+        this.marque = pmarque;
+        this.nom = pnom;
+        this.prix = pprix;
+    }
+    public Composant copier(){
+        String copieNom = this.nom;
+        String copieCategorie = this.categorie;
+        String copieMarque = this.marque;
+        double copiePrix = this.prix;
+        Composant copie = new Composant(copieCategorie, copieMarque, copieNom, copiePrix);
+        return copie;
+    }
+    public boolean estIdentique(Composant autre){
+        if(categorie == autre.getCategorie()){
+            if(marque == autre.getMarque()) {
+                if(nom == autre.getNom()) {
+                    return true;
+                }
+                else{ return false;}
+            }
+            else{ return false;}
+        }
+        return false;
+    }
+    public String toString(){
+        return "[" + categorie + "] " + marque + " " + nom;
+    }
+    public String getNom(){
+        return nom;
+    }
+    public String getCategorie(){
+        return categorie;
+    }
+    public String getMarque(){
+        return marque;
+    }
+    public double getPrix(){
+        double rab = 1 - rabais;
+        return (this.prix * rab);
+    }
+
+    public double getRabais() {
+        return rabais;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setCategorie(String categorie) {
+        this.categorie = categorie;
+    }
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+    public void setRabais(double rabais) {
+        this.rabais = rabais;
+    }
 }
--- src/Configuration.java
+++ src/Configuration.java
@@ -1,3 +1,76 @@
 public class Configuration {
-    
+    private String description;
+    private double prixMax;
+    private Composant [] composants;
+    private int nmbComposants;
+    private final int maxComposants = 20;
+
+    public Configuration(String description, double prixMax, Composant[] composants){
+        this.description = description.toUpperCase();
+        this.prixMax = prixMax;
+        this.composants = composants;
+    }
+    public Configuration(Configuration originale){
+
+    }
+    public double calculerTotal(double taxe){
+        double prix = 0;
+        Composant nom;
+        if(composants[0] != null) {
+            for (int i = 0; i < composants.length; i++) {
+                nom = composants[i];
+                prix =+ nom.getPrix();
+            }
+        }
+        return prix;
+    }
+    public Composant rechercher(String categorie){
+        String categoriePiece;
+        if(composants[0] != null) {
+            for (int i = 0; i < composants.length; i++) {
+                 categoriePiece = composants[i].getCategorie();
+                 if(categoriePiece == categorie){
+                     return composants[i];
+                 }
+            }
+        }
+        return null;
+    }
+    public boolean ajouter(Composant composant){
+        String categorie = composant.getCategorie();
+    if(maxComposants > nmbComposants){
+        for(int i = 0; i < composants.length; i++){
+            if(categorie == composants[i].getCategorie()){
+                return false;
+            }
+            else{
+                for(int x = 0; x < composants.length; i++){
+                    if(composants[x] == null){
+                        composants[x] = composant;
+                        if(calculerTotal(0.15) > prixMax){
+                            composants[x] = null;
+                            return false;
+                        }
+                        else{
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return false;
+    }
+    public boolean retirer(Composant composant) {
+
+    }
+    public boolean remplacer(Composant composant){
+
+    }
+    public String toString() {
+
+    }
+
+    public Composant[] getComposants() { return composants; }
+    public int getNbComposants(){ return nmbComposants; }
 }
